92b4961853ac36f6bd1bc60abaaecbd7c167e503
dralt10 <dralt6@gmail.com>
2026-07-20 11:24:00 +0530

    Create detox.js

---
+++ detox.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const beginButton = document.getElementById("beginButton");
+
+    if (beginButton) {
+
+        beginButton.addEventListener("click", function () {
+
+            alert("Patient Details screen will open next.");
+
+        });
+
+    }
+
+});
