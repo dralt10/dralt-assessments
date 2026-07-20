bc65a1df9bedb3174b8142f1b50b49ece7bd2da5
dralt10 <dralt6@gmail.com>
2026-07-20 14:46:40 +0530

    Update detox.js

--- detox.js
+++ detox.js
@@ -97,16 +97,9 @@ document.getElementById("assessmentDate").textContent =
 <h4>${questions[questionNumber - 1].hindi}</h4>
 <div class="score-buttons">
 
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>10</button>
+    ${Array.from({ length: 10 }, (_, i) => `
+        <button>${i + 1}</button>
+    `).join("")}
 
 </div>
 
