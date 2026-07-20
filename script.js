b3951ec5aff71730ffe76038f0cb9c7880e37476
dralt10 <dralt6@gmail.com>
2026-07-20 09:36:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const startButton = document.getElementById("startButton");
+
+if(startButton){
+
+startButton.addEventListener("click", function(){
+
+alert("Welcome to the DRALT Health Assessment Engine.\n\nThe Detox Health Score will be added in the next step.");
+
+});
+
+}
+
+});
